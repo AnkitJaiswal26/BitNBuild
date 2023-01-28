a2ec5502c9a209a55a3ec843d7d6ea82d7c4bd3f
atharvapatil123 <atharva7275@gmail.com>
2023-01-28 23:32:08 +0530

    Integrating SC

--- bit-n-build/src/pages/Admin/Admin.jsx
+++ bit-n-build/src/pages/Admin/Admin.jsx
@@ -4,94 +4,136 @@ import { useNavigate } from "react-router-dom";
 import { useCallback, useState, useEffect, useRef } from "react";
 import MoonLoader from "react-spinners/MoonLoader";
 import { ToastContainer, toast } from "react-toastify";
-import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import DoneRoundedIcon from "@mui/icons-material/DoneRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import axios from "axios";
+import { useAuth } from "../../Context/AuthContext";
+import { useSafeBuyContext } from "../../Context/SafeBuyContext";
 
 const Admin = () => {
   const navigate = useNavigate();
+  const { checkIfWalletConnected, currentAccount } = useAuth();
 
-    const companies = [
-        {
-            name: "Boat",
-            cin: "U74999UP2016PTC084312", 
-            category: "Electronics"
-        },
-        {
-            name: "Samsung",
-            cin: "U74999UP2016PTC084312",
-            category: "Electronics"
-        },
-        {
-            name: "Britania 50-50",
-            cin: "U74999UP2016PTC084312",
-            category: "Snacks"
-        }
-    ];
-    const requests = [];
-    const requestType = "Update"
+  useEffect(() => {
+    checkIfWalletConnected();
+    fetchRequests();
+  }, []);
+
+  const [requests, setRequests] = useState([]);
+  const { fetchActiveRequests, acceptCompany, rejectCompany } =
+    useSafeBuyContext();
+
+  const fetchRequests = useCallback( async (e) => {
+    console.log("Hello Admin :)");
+    e.preventDefault();
+    try {
+      const data = await fetchActiveRequests();
+      setRequests(data);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const acceptComp = useCallback( async (e, comAdd) => {
+    console.log("Hello verifier, accept me :)");
+    e.preventDefault();
+    try {
+      await acceptCompany(comAdd);
+      console.log("Company Accepted");
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const rejectComp = useCallback( async (e, comAdd) => {
+    console.log("Hello verifier, reject me :(");
+    e.preventDefault();
+    try {
+      await rejectCompany(comAdd);
+      console.log("Company Rejected");
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  // const companies = [
+  //     {
+  //         name: "Boat",
+  //         cin: "U74999UP2016PTC084312",
+  //         category: "Electronics"
+  //     },
+  //     {
+  //         name: "Samsung",
+  //         cin: "U74999UP2016PTC084312",
+  //         category: "Electronics"
+  //     },
+  //     {
+  //         name: "Britania 50-50",
+  //         cin: "U74999UP2016PTC084312",
+  //         category: "Snacks"
+  //     }
+  // ];
 
   return (
     <>
       <ToastContainer />
-        <div className={styles.companyDashboardContainer}>
-          <div className={styles.dashboardBox}>
-
-            <div className={styles.detailsBox}>
-              <div className={styles.detailsHeading}>
-                <span>Requests</span>
-              </div>
-              {companies.length > 0 ? (
-                <>
-                  <div className={styles.docCardHeader}>
-                    <span className={styles.docCardContent}>Company Name</span>
-                    <span className={styles.docCardContent}>Company Identification Number</span>
-                    <span className={styles.docCardContent}>Category</span>
-                    <span className={styles.docCardContent}>Verify</span>
-                  </div>
-                  {companies.map((item, index) => {
-                    return (
-                      <div
-                        className={
-                          index % 2 == 0
-                            ? `${styles.docCard} ${styles.evenDocCard}`
-                            : `${styles.docCard} ${styles.oddDocCard}`
-                        }
-                      >
-                        <span className={styles.docCardContent}>
-                          {item.name}
-                        </span>
-                        <span className={styles.docCardContent}>
-                          {item.cin}
-                        </span>
-                        <span className={styles.docCardContent}>
-                          {item.category}
-                        </span>
-                        <span className={styles.docCardContent}>
-                        <button className={styles.viewAllBtn} 
-                            onClick={() => {
-                            //   Verify Company;
-                            }}
-                        ><DoneRoundedIcon /></button>
-                        <button className={styles.viewAllBtn}
-                            onClick={() => {
-                            //   Reject Company;
-                            }}
-                        ><CloseRoundedIcon /></button>
-                        </span>
-                      </div>
-                    );
-                  })}
-                </>
-              ) : (
-                <span className={styles.emptyListMessage}>
-                  No response found
-                </span>
-              )}
+      <div className={styles.companyDashboardContainer}>
+        <div className={styles.dashboardBox}>
+          <div className={styles.detailsBox}>
+            <div className={styles.detailsHeading}>
+              <span>Requests</span>
             </div>
-
-            </div>
+            {requests.length > 0 ? (
+              <>
+                <div className={styles.docCardHeader}>
+                  <span className={styles.docCardContent}>Company Name</span>
+                  <span className={styles.docCardContent}>
+                    Company Identification Number
+                  </span>
+                  <span className={styles.docCardContent}>Category</span>
+                  <span className={styles.docCardContent}>Verify</span>
+                </div>
+                {requests.map((item, index) => {
+                  return (
+                    <div
+                      className={
+                        index % 2 == 0
+                          ? `${styles.docCard} ${styles.evenDocCard}`
+                          : `${styles.docCard} ${styles.oddDocCard}`
+                      }
+                    >
+                      <span className={styles.docCardContent}>{item.name}</span>
+                      <span className={styles.docCardContent}>{item.cin}</span>
+                      <span className={styles.docCardContent}>
+                        {item.category}
+                      </span>
+                      <span className={styles.docCardContent}>
+                        <button
+                          className={styles.viewAllBtn}
+                          onClick={(e) => {
+                            acceptComp(e, item.comAdd);
+                          }}
+                        >
+                          <DoneRoundedIcon />
+                        </button>
+                        <button
+                          className={styles.viewAllBtn}
+                          onClick={(e) => {
+                            rejectComp(e, item.comAdd);
+                          }}
+                        >
+                          <CloseRoundedIcon />
+                        </button>
+                      </span>
+                    </div>
+                  );
+                })}
+              </>
+            ) : (
+              <span className={styles.emptyListMessage}>No response found</span>
+            )}
+          </div>
         </div>
+      </div>
     </>
   );
 };
